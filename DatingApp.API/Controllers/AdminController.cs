91e26e81ed725749cf13143dda9b857c34e4c991
addna <addna@DESKTOP-MFNARD1>
2019-07-31 18:21:28 +0200

    Returning the users with their roles

--- DatingApp.API/Controllers/AdminController.cs
+++ DatingApp.API/Controllers/AdminController.cs
@@ -1,5 +1,9 @@
+using DatingApp.API.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace DatingApp.API.Controllers
 {
@@ -7,15 +11,34 @@ namespace DatingApp.API.Controllers
     [Route("api/[controller]")]
     public class AdminController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public AdminController(DataContext context)
+        {
+            _context = context;
+        }
+
         [Authorize(Policy = "RequireAdminRole")]
         [HttpGet("usersWithRoles")]
-        public IActionResult GetUsersWithRoles()
+        public async Task<IActionResult> GetUsersWithRoles()
         {
-            return Ok("Only admins can see this");
+            var userList = await (from user in _context.Users orderby user.UserName
+                                    select new 
+                                    {
+                                        Id = user.Id,
+                                        UserName = user.UserName,
+                                        Roles = (from userRole in user.UserRoles
+                                                    join role in _context.Roles
+                                                    on userRole.RoleId
+                                                    equals role.Id
+                                                    select role.Name).ToList()
+                                    }).ToListAsync();
+
+            return Ok(userList);
         }
 
         [Authorize(Policy = "ModeratePhotoRole")]
-        [HttpGet("photoForModerations")]
+        [HttpGet("photosForModeration")]
         public IActionResult GetPhotosForModeration()
         {
             return Ok("Amins and moderators can see this");
